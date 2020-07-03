8c1865c86781e93a4d6f0ab443f3143c9d3ab8e9
SeonHyungJo <seonhyung.jo@gmail.com>
2020-07-03 22:43:50 +0900

    fix : 커밋 메시지, 링크 디코딩 수정 및 버그 처리

--- utils/update-readme.js
+++ utils/update-readme.js
@@ -7,38 +7,35 @@ const { getTitleFromUrl } = require('./url')
 
 const OWNER = 'im-d-team'
 const REPO_NAME = 'Dev-Contents-House'
+const TARGET_PATH = 'README.md';
+const LinkLocTarget = '## 📖 This Week';
+const TOKEN = process.env.GITHUB_TOKEN || ''
 
 async function createNewContent({ user_name, text: targetUrl }) {
 
   try {
-    const token = await process.env.GITHUB_TOKEN || ''
-
-    if (!token) {
-      throw Error('token is empty');
+    if (!TOKEN) {
+      throw Error('TOKEN is empty');
     }
 
-    const linkLocTarget = '## 📖 This Week';
-    const TARGET_PATH = 'README.md';
-    console.log('body', process.env.GITHUB_TOKEN)
-
-    const octokit = github.getOctokit(token);
+    const octokit = github.getOctokit(TOKEN);
+    const realURL = decodeURIComponent(targetUrl)
 
     // Get Our Repo README.md
     const { data: { content, sha } } = await getReadme(octokit, OWNER, REPO_NAME)
-    // Get title from url
-    const contentTitle = await getTitleFromUrl(decodeURIComponent(targetUrl))
 
-    console.log('content', content)
-    const fileLinkContent = `- [${contentTitle}](${targetUrl})`
+    // Get title from url
+    const contentTitle = await getTitleFromUrl(realURL)
+    const fileLinkContent = `- [${contentTitle}](${realURL})`
 
     const originalContent = Buffer.from(content, 'base64').toString('utf8');
-    const splitContent = originalContent.split(linkLocTarget)
+    const splitContent = originalContent.split(LinkLocTarget)
 
     // Create New README Content
-    const newContent = Buffer.from(splitContent[0] + linkLocTarget + '\n' + fileLinkContent + '\n' + splitContent[1], 'utf8').toString('base64');
+    const newContent = Buffer.from(splitContent[0] + LinkLocTarget + '\n' + fileLinkContent + splitContent[1], 'utf8').toString('base64');
 
     // Update README
-    await updateReadme(octokit, OWNER, REPO_NAME, TARGET_PATH, `:house: ${user_name} / contentTitle`, newContent, sha)
+    updateReadme(octokit, OWNER, REPO_NAME, TARGET_PATH, `:house: ${user_name} / ${contentTitle}`, newContent, sha)
   }
   catch (error) {
     core.setFailed(error.message);
